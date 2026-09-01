7bb1252be8f011f44658d4a9372db9da6c2e2ab6
Nimco <nimcokayf666@gmail.com>
2026-09-01 19:12:18 +0300

    execise1 iso dabac

---
+++ execise1/index.js
@@ -0,0 +1,76 @@
+// console.log("helow guys");
+
+// 1 cheak age
+let age = 20;
+
+if(age >=18){
+    console.log("you are eglible to access the system.")
+}else{
+    console.log("you are not  eglible to access the system.")
+}
+
+// 2 cheak graide
+let score = 65;
+
+if (score >=70) {
+    console.log("Grade B");
+} else if (score >= 70) {
+    console.log("Grade C");
+} else if (score >= 50) {
+    console.log("Grade C-");
+} else {
+    console.log("Failed");
+}
+
+// 3 cheak number
+let number = 5;
+
+if (number > 0) {
+    console.log("Positive");
+} else if (number < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
+// 4 cheak isloggedin
+let isLoggedIn = false;
+
+if (!isLoggedIn) {
+    console.log("You are not logged in.");
+} else {
+    console.log("Welcome! You are logged in.");
+}
+// 5 cheak ===
+
+console.log("10" === 10);
+console.log("10" === "10");
+
+// 6 cheak data type
+
+let name = "Nimco";
+console.log(typeof name);
+let Age = 21;
+console.log(typeof age);
+let IsLoggedIn = true;
+console.log(typeof isLoggedIn);
+
+// 7 cheak add,sub,mul,div
+
+let num1 = 20;
+let num2 = 5;
+
+let addition = num1 + num2;
+console.log( addition);
+let subtraction = num1 - num2;
+console.log( subtraction);
+let multiplication = num1 * num2;
+console.log( multiplication);
+let division = num1 / num2;
+console.log( division);
+
+
+
+
+
+
+
